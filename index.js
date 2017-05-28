a7de312fe96f6a1db6796d618bd5b4f4fa7cce4d
Jason Mulligan <jason.mulligan@avoidwork.com>
2017-05-27 20:43:32 -0400

    Moving middleware to it's own file, adding `allowed` middleware to short circuit the error instead of going through all of the middleware, removing the first condition from the `last()` middleware handler

--- index.js
+++ index.js
@@ -2,25 +2,10 @@
 
 const path = require("path"),
 	each = require("retsu").each,
-	regex = require(path.join(__dirname, "lib", "regex.js")),
+	middleware = require(path.join(__dirname, "lib", "middleware.js")),
 	Woodland = require(path.join(__dirname, "lib", "woodland.js")),
 	max = 1000,
 	random = Math.floor(Math.random() * max) + 1;
-
-function valid (req, res, next) {
-	const pathname = req.parsed.pathname.replace(regex.root, ""),
-		invalid = (pathname.replace(regex.dir, "").split("/").filter(i => i !== ".")[0] || "") === "..",
-		outDir = !invalid ? (pathname.match(/\.{2}\//g) || []).length : 0,
-		inDir = !invalid ? (pathname.match(/\w+?(\.\w+|\/)+/g) || []).length : 0;
-
-	if (invalid) {
-		next(new Error(404));
-	} else if (outDir > 0 && outDir >= inDir) {
-		next(new Error(404));
-	} else {
-		next();
-	}
-}
 
 function factory ({cacheSize = max, defaultHeaders = {}, defaultHost = "localhost", hosts = ["localhost"], seed = random} = {}) {
 	const router = new Woodland(defaultHost, defaultHeaders, cacheSize, seed);
@@ -28,7 +13,8 @@ function factory ({cacheSize = max, defaultHeaders = {}, defaultHost = "localhos
 	router.route = router.route.bind(router);
 	router.setHost("all");
 	each(hosts, host => router.setHost(host));
-	router.use("/.*", valid, "all", "all").blacklist(valid);
+	router.use("/.*", middleware.allowed, "all", "all").blacklist(middleware.allowed);
+	router.use("/.*", middleware.valid, "all", "all").blacklist(middleware.valid);
 
 	return router;
 }
