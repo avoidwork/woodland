042fc3c49ea4ee71c1e9451aeec0684e0c639500
Jason Mulligan <jason.mulligan@avoidwork.com>
2017-07-07 08:53:03 -0400

    Making `valid()` middleware a little more robust & removing the need to validate the request method later in the middleware

--- index.js
+++ index.js
@@ -8,17 +8,21 @@ const path = require("path"),
 	random = Math.floor(Math.random() * max) + 1;
 
 function valid (req, res, next) {
-	const pathname = req.parsed.pathname.replace(regex.root, ""),
-		invalid = (pathname.replace(regex.dir, "").split("/").filter(i => i !== ".")[0] || "") === "..",
-		outDir = !invalid ? (pathname.match(/\.{2}\//g) || []).length : 0,
-		inDir = !invalid ? (pathname.match(/\w+?(\.\w+|\/)+/g) || []).length : 0;
+	if (req.allow.indexOf(req.method) > -1) {
+		const pathname = req.parsed.pathname.replace(regex.root, ""),
+			invalid = (pathname.replace(regex.dir, "").split("/").filter(i => i !== ".")[0] || "") === "..",
+			outDir = !invalid ? (pathname.match(/\.{2}\//g) || []).length : 0,
+			inDir = !invalid ? (pathname.match(/\w+?(\.\w+|\/)+/g) || []).length : 0;
 
-	if (invalid) {
-		next(new Error(404));
-	} else if (outDir > 0 && outDir >= inDir) {
-		next(new Error(404));
+		if (invalid) {
+			next(new Error(404));
+		} else if (outDir > 0 && outDir >= inDir) {
+			next(new Error(404));
+		} else {
+			next();
+		}
 	} else {
-		next();
+		next(new Error(req.allow.length > 0 ? 405 : 404));
 	}
 }
 
