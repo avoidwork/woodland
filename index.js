d40513c71a86a46854d6f9c587d421006125902c
Jason Mulligan <jason.mulligan@avoidwork.com>
2017-10-16 19:49:10 -0400

    More purging

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 
 const path = require("path"),
 	all = require(path.join(__dirname, "lib", "all.js")),
+	valid = require(path.join(__dirname, "lib", "valid.js")),
 	Woodland = require(path.join(__dirname, "lib", "woodland.js"));
 
 function factory ({cacheSize = 1000, coerce = true, defaultHeaders = {}, seed = Math.floor(Math.random() * 1000) + 1} = {}) {
 	const router = new Woodland(defaultHeaders, cacheSize, seed, coerce);
 
-	router.route = router.route.bind(router);
-	router.use("/*", valid, all, all).blacklist(valid);
+	router.use("/.*", valid, all).blacklist(valid);
 
 	return router;
 }
