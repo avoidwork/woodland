9fcec6a2b6c1c0394eb7e79036f89d4a5614d098
Jason Mulligan <jason.mulligan@avoidwork.com>
2017-10-16 20:51:43 -0400

    Removing unneeded middleware which drops first hop for all routes

--- index.js
+++ index.js
@@ -2,14 +2,12 @@
 
 const path = require("path"),
 	all = require(path.join(__dirname, "lib", "all.js")),
-	valid = require(path.join(__dirname, "lib", "valid.js")),
 	Woodland = require(path.join(__dirname, "lib", "woodland.js"));
 
 function factory ({cacheSize = 1000, coerce = true, defaultHeaders = {}, seed = Math.floor(Math.random() * 1000) + 1} = {}) {
 	const router = new Woodland(defaultHeaders, cacheSize, seed, coerce);
 
 	router.route = router.route.bind(router);
-	router.use("/.*", valid, all).blacklist(valid);
 
 	return router;
 }
