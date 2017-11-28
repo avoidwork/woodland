ca8b2a3b1c171af27037c290289682b3e5947677
Jason Mulligan <jason.mulligan@avoidwork.com>
2017-11-28 08:18:14 -0500

    Updating sample & test files to use new helper functions, fixing `res.send()` for `http1` requests

--- sample.js
+++ sample.js
@@ -3,7 +3,7 @@
 const http = require("http"),
 	router = require("./index")({defaultHeaders: {"Cache-Control": "no-cache", "Content-Type": "text/plain"}});
 
-router.use("/", (req, res) => res.end("Hello World!"));
-router.use("/", (req, res) => res.end("Make a GET request to retrieve the representation"), "options");
-router.use("/:user", (req, res) => res.end("Hello " + req.params.user + "!"));
+router.use("/", (req, res) => res.send("Hello World!"));
+router.use("/", (req, res) => res.send("Make a GET request to retrieve the representation"), "options");
+router.use("/:user", (req, res) => res.send("Hello " + req.params.user + "!"));
 http.createServer(router.route).listen(8000);
