484f10db138c08ce1906cc0102376cb8a58a7705
Jason Mulligan <jason.mulligan@avoidwork.com>
2017-12-20 11:59:53 -0500

    Refactoring `route()` into a facade

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require("path"),
 function factory ({cacheSize = 1000, coerce = true, defaultHeaders = {}, seed = Math.floor(Math.random() * 1000) + 1, http2 = false} = {}) {
 	const router = new Woodland(defaultHeaders, cacheSize, seed, coerce, http2);
 
-	router.route = router.route.bind(router);
+	router.route = (...args) => router._route.apply(router, args);
 
 	return router;
 }
