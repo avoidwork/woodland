a7a53408b0aa4d65ded117875e7b80584e538a3c
Jason Mulligan <jason.mulligan@avoidwork.com>
2020-12-28 17:26:42 -0500

    Setting `content-length` response header when absent, updating `sample.js` with error route

--- sample.js
+++ sample.js
@@ -5,5 +5,6 @@ const http = require("http"),
 
 router.use("/", (req, res) => res.send("Hello World!"));
 router.use("/", (req, res) => res.send("Make a GET request to retrieve the representation"), "options");
+router.use("/error", (req, res) => res.error(404));
 router.use("/:user", (req, res) => res.send("Hello " + req.params.user + "!"));
 http.createServer(router.route).listen(8000);
