14ac5babda602495f217284fd9b2d3458ec491f7
Jason Mulligan <jason.mulligan@avoidwork.com>
2021-01-01 12:27:58 -0500

    Updating sample.js

--- sample.js
+++ sample.js
@@ -4,9 +4,9 @@ const http = require("http"),
 	{join} = require("path"),
 	router = require("./index")({autoindex: true, defaultHeaders: {"cache-control": "no-cache", "content-type": "text/plain; charset=utf-8"}, time: true});
 
-router.use("/", (req, res) => res.send("Hello World!"));
-router.use("/", (req, res) => res.send("Make a GET request to retrieve the representation"), "options");
-router.use("/error", (req, res) => res.error(404));
-router.use("/test(/.*)?", (req, res) => router.serve(req, res, req.parsed.pathname.replace(/^\/test\/?/, ""), join(__dirname, "test")));
-router.use("/:user", (req, res) => res.send("Hello " + req.params.user + "!"));
+router.get("/", (req, res) => res.send("Hello World!"));
+router.options("/", (req, res) => res.send("Make a GET request to retrieve the representation"));
+router.get("/error", (req, res) => res.error(404));
+router.get("/test(/.*)?", (req, res) => router.serve(req, res, req.parsed.pathname.replace(/^\/test\/?/, ""), join(__dirname, "test")));
+router.get("/:user", (req, res) => res.send("Hello " + req.params.user + "!"));
 http.createServer(router.route).listen(8000);
