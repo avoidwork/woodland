21cb8ab2a16fbdfac5fb97074bf775e3ed26bc81
Jason Mulligan <jason.mulligan@avoidwork.com>
2021-01-01 19:23:12 -0500

    Fixing call to `log()` within `res.send()`

--- benchmarks/woodland.js
+++ benchmarks/woodland.js
@@ -2,7 +2,7 @@
 
 const path = require("path"),
 	http = require("http"),
-	router = require(path.join(__dirname, "..", "index.js"))();
+	router = require(path.join(__dirname, "..", "index.js"))({logging:{enabled: false}});
 
 router.use("/", (req, res) => res.json("Hello World!"));
 http.createServer(router.route).listen(8000);
