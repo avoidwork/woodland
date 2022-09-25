b6f5dbf4e04e4a0aa603790051b8e8ba19a513ba
Jason Mulligan <jason.mulligan@avoidwork.com>
2022-09-24 20:38:42 -0400

    Fixing package.json `files` value, generating `types` files

---
+++ types/constants.d.ts
@@ -0,0 +1,13 @@
+export const all: "*";
+export const delimiter: "|";
+export namespace levels {
+    const emerg: number;
+    const alert: number;
+    const crit: number;
+    const error: number;
+    const warn: number;
+    const notice: number;
+    const info: number;
+    const debug: number;
+}
+export const months: string[];
