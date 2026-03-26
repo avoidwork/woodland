0da7be7715aa78bc76bd3e23678662dd73ee598a
Jason Mulligan <jason.mulligan@avoidwork.com>
2026-03-26 08:18:33 -0400

    test: merge responses.test.js into response.test.js

--- tests/unit/response.test.js
+++ tests/unit/response.test.js
@@ -15,6 +15,12 @@ import {
 	partialHeaders,
 	pipeable,
 	writeHead,
+	createErrorHandler,
+	createJsonHandler,
+	createRedirectHandler,
+	createSendHandler,
+	createSetHandler,
+	createStatusHandler,
 } from "../../src/response.js";
 
 describe("response", () => {
@@ -1307,6 +1313,135 @@ describe("response", () => {
 			writeHead(res, {});
 
 			assert.strictEqual(called, true);
+		});
+	});
+
+	describe("createErrorHandler", () => {
+		it("should create error handler that calls send", () => {
+			let sendCalled = false;
+			let errorEmitted = false;
+			let contentLengthRemoved = false;
+
+			const req = { parsed: { pathname: "/test" }, method: "GET", ip: "127.0.0.1" };
+			const res = {
+				statusCode: 200,
+				headersSent: false,
+				removeHeader: (name) => {
+					if (name === "content-length") {
+						contentLengthRemoved = true;
+					}
+				},
+				send: () => {
+					sendCalled = true;
+				},
+			};
+			const emitter = {
+				emit: () => {
+					errorEmitted = true;
+				},
+			};
+
+			const errorHandler = createErrorHandler(req, res, emitter);
+			errorHandler(404, "Not found");
+
+			assert.ok(sendCalled);
+			assert.ok(errorEmitted);
+			assert.ok(contentLengthRemoved);
+		});
+	});
+
+	describe("createJsonHandler", () => {
+		it("should create json handler that calls send", () => {
+			let sendCalled = false;
+
+			const res = {
+				statusCode: 200,
+				send: () => {
+					sendCalled = true;
+				},
+			};
+
+			const jsonHandler = createJsonHandler(res);
+			jsonHandler({ test: "data" });
+
+			assert.ok(sendCalled);
+		});
+	});
+
+	describe("createRedirectHandler", () => {
+		it("should create redirect handler that calls send", () => {
+			let sendCalled = false;
+
+			const res = {
+				send: () => {
+					sendCalled = true;
+				},
+			};
+
+			const redirectHandler = createRedirectHandler(res);
+			redirectHandler("/new-location", true);
+
+			assert.ok(sendCalled);
+		});
+	});
+
+	describe("createSendHandler", () => {
+		it("should create send handler", () => {
+			let onReadyCalled = false;
+			let onDoneCalled = false;
+
+			const req = { method: "GET", headers: {} };
+			const res = {
+				statusCode: 200,
+				headersSent: false,
+			};
+
+			const sendHandler = createSendHandler(
+				req,
+				res,
+				() => {
+					onReadyCalled = true;
+					return ["body", 200, {}];
+				},
+				() => {
+					onDoneCalled = true;
+				},
+			);
+
+			sendHandler("test body");
+
+			assert.ok(onReadyCalled);
+			assert.ok(onDoneCalled);
+		});
+	});
+
+	describe("createSetHandler", () => {
+		it("should create set handler that calls setHeader", () => {
+			let setHeaderCalled = false;
+
+			const res = {
+				setHeader: () => {
+					setHeaderCalled = true;
+				},
+			};
+
+			const setHandler = createSetHandler(res);
+			setHandler({ "x-custom": "value" });
+
+			assert.ok(setHeaderCalled);
+		});
+	});
+
+	describe("createStatusHandler", () => {
+		it("should create status handler that sets statusCode", () => {
+			const res = {
+				statusCode: 200,
+			};
+
+			const statusHandler = createStatusHandler(res);
+			statusHandler(404);
+
+			assert.strictEqual(res.statusCode, 404);
 		});
 	});
 });
